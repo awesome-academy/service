eab648f1886833128fb228354281489e27587d09
quanghung97 <hungcan1997@gmail.com>
2019-03-15 16:09:15 +0700

    add Model and relationship

---
+++ app/Models/Location.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'min_people',
+        'max_people',
+        'status_id',
+    ];
+
+    /**
+     * A services can be applied to locations.
+     */
+    public function typeServices()
+    {
+        return $this->belongsToMany('App\Models\TypeService', 'location_has_services');
+    }
+
+    /**
+     * A status can be applied to location.
+     */
+    public function status()
+    {
+        return $this->belongsTo('App\Models\Status');
+    }
+
+    /**
+     * A location has one confirmation.
+     */
+    public function confirmation()
+    {
+        return $this->hasOne('App\Models\Confirmation');
+    }
+}
